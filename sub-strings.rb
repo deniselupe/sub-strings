52c017f399d092e41242adffcf06738d67828772
deniselupe <dgrodriguez24@hotmail.com>
2021-12-03 17:44:50 +0000

    feat: print uniq count for substrings in message

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,27 +1,11 @@
-=begin
-This code is written assuming that both argument values
-will be of the string data type.
-
-The return object will only show if every element listed in the 
-array_of_sub_strings is included at least once in the string provided.
-
-This does not currently count the number of times the substring 
-is included within the string provided.
-=end
-
 def substrings(message, array_of_sub_strings)
   uniq_count = array_of_sub_strings.reduce({}) do |obj, val|
-    if !obj[val]
-      obj[val] = 0
-    end
-    
-    if message.downcase.include?(val)
-      obj[val] += 1
-    end
-    
+    obj[val] = message.downcase.scan(val).length
     obj
   end
   
+  # Filter out the Objects with Value of 0
+  uniq_count = uniq_count.select { |obj, val| val > 0 }
   puts uniq_count
 end
 
